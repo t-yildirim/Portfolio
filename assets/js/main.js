8ff941974a39e82e2eb42c36d1e6fd5870282f84
Tuncay Yildirim <tyildirim@192.168.1.6>
2025-04-26 17:29:23 +0300

    website updated for fixing errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,19 +72,7 @@ modalCloses.forEach((modalClose) => {
     })
 })
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiperPortfolio = new Swiper('.portfolio__container', {
-    cssMode: true,
-    loop: true,
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+// Removed Swiper initialization since we're using a grid layout now
 
 /*==================== TESTIMONIAL ====================*/
 let swiperTestimonial = new Swiper('.testimonial__container', {
